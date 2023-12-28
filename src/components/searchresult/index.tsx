f4f456a928013a4cb545574a70296231c5eab04d
hmdev <=>
2023-12-28 00:19:34 -0300

    ...

--- src/components/searchresult/index.tsx
+++ src/components/searchresult/index.tsx
@@ -15,7 +15,7 @@ function SearchResult() {
         <div className="containerResults">
              <ul className="ulClients">
                 {context?.namesResult.map((el)=>
-                    <li className="itemList" key={el[0]} onClick={(e)=>autoCompleteInputName(el)}>{el[1]}</li>
+                    <li className="itemList" key={el[0]} onClick={()=>autoCompleteInputName(el)}>{el[1]}</li>
                 )}
              </ul>
         </div>
